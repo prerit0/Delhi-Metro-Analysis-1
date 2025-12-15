c7f12081224702c9efca117ab0cc4ae5ca4402c7
prerit0 <masihprerit@gmail.com>
2025-12-15 23:01:20 +0530

    edited

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,20 @@
 import streamlit as st
 import pandas as pd
-st.title("Data Load Test")
+import matplotlib.pyplot as plt
+import seaborn as sns 
 
-df = pd.read_csv("delhi_metro_cleaned_Dataset.csv")
-st.dataframe(df.head())
+st.title("Delhi Metro Analysis")
+
+@st.cache_data
+def load_data():
+    return pd.read_csv("delhi_metro_cleaned_Dataset.csv")
+
+df = load_data()
+
+plt.figure(figsize=(8,5))
+plt.hist(df['Passengers'], bins=20, color = 'yellow', edgecolor = 'black')
+plt.title('Passengers Distribution')
+plt.xlabel('Passengers per Trip')
+plt.ylabel('Frequency')
+st.pyplot()
 
